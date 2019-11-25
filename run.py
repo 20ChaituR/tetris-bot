fixed rotation

run.py
@@ -1,83 +1,57 @@
 import pyglet
 import torch
-import torch.nn as nn
-from torch.autograd import Variable
-from torch.distributions import Categorical
 
 import game
+import train
 
-model = 'models/tetris_policy_100.pth'
-
-
-class Policy(nn.Module):
-    def __init__(self):
-        super(Policy, self).__init__()
-        self.state_space = game.state_size
-        self.action_space = game.action_size
-
-        self.l1 = nn.Linear(self.state_space, 128, bias=False)
-        self.l2 = nn.Linear(128, self.action_space, bias=False)
-
-        self.gamma = 0.99
-
-        # Episode policy and reward history
-        self.policy_history = Variable(torch.Tensor())
-        self.reward_episode = []
-        # Overall reward and loss history
-        self.reward_history = []
-        self.loss_history = []
-
-    def forward(self, x):
-        model = torch.nn.Sequential(
-            self.l1,
-            nn.Dropout(p=0.6),
-            nn.ReLU(),
-            self.l2,
-            nn.Softmax(dim=-1)
-        )
-        return model(x)
+model = 'models/tetris_policy_500.pth'
 
 
-def select_action(state):
-    # Select an action (0 or 1) by running policy model and choosing based on the probabilities in state
-    state = torch.from_numpy(state).type(torch.FloatTensor)
-    state = policy(Variable(state))
-    c = Categorical(state)
-    action = c.sample()
-    return action
-
-
-policy = Policy()
+policy = train.Policy()
 policy.load_state_dict(torch.load(model))
 
 state = game.reset()
+score = 0
+high_score = 0
+
+last_move = 0
 
 # Set constants for the screen size
-SCREEN_WIDTH = 100
-SCREEN_HEIGHT = 180
+GAME_WIDTH = 100
+UI_WIDTH = 150
+GAME_HEIGHT = 180
 SQUARE_WIDTH = 10
 SQUARE_HEIGHT = 10
+
 SCREEN_TITLE = "Tetris"
 
-window = pyglet.window.Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+window = pyglet.window.Window(GAME_WIDTH + UI_WIDTH, GAME_HEIGHT)
+pyglet.gl.glClearColor(1,1,1,1)
 window.clear()
 
 
 def update_frame(x, y):
-    global state
-    action = select_action(state)
-    state, _, done = game.step(action.item())
+    global state, score, high_score, last_move
+
+    action = train.select_action(state)
+    state, reward, done = game.step(action.item())
+    score += reward
+    last_move = action.item()
 
     if done:
         game.reset()
+        high_score = max(high_score, score)
+        score = 0
 
 
 @window.event
 def on_draw():
+    global score, high_score, last_move
+
     window.clear()
 
     color_map = {
-        '': (0, 0, 0),
+        '': (0,0,0),
         "L": (15, 61, 243),
         "S": (92, 195, 243),
         "I": (243, 112, 32),
@@ -88,12 +62,64 @@ def on_draw():
 
     for i in range(18):
         for j in range(10):
-            x = SCREEN_WIDTH - SQUARE_WIDTH * j
-            y = SCREEN_HEIGHT - SQUARE_HEIGHT * i
+            x = SQUARE_WIDTH * (j + 1)
+            y = GAME_HEIGHT - SQUARE_HEIGHT * i
             dx = SQUARE_WIDTH
             dy = SQUARE_HEIGHT
             pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [x, y, x - dx, y, x - dx, y - dy, x, y - dy]),
                                  ('c3B', color_map[game.grid[i][j]] * 4))
+
+    font_size = 11
+
+    label = pyglet.text.Label('High Score: ' + str(high_score),
+                              font_name='Times New Roman',
+                              font_size=font_size,
+                              color = (0,0,0,255),
+                              x=GAME_WIDTH + UI_WIDTH // 2, y=GAME_HEIGHT - 2 * font_size,
+                              anchor_x='center', anchor_y='center')
+    label.draw()
+
+    label = pyglet.text.Label('Score: ' + str(score),
+                              font_name='Times New Roman',
+                              font_size=font_size,
+                              color=(0, 0, 0, 255),
+                              x=GAME_WIDTH + UI_WIDTH // 2, y=GAME_HEIGHT - 4 * font_size,
+                              anchor_x='center', anchor_y='center')
+    label.draw()
+
+    x = GAME_WIDTH + 3 * UI_WIDTH // 4
+    y = GAME_HEIGHT - 6 * font_size
+    dx = UI_WIDTH // 2
+    dy = 2 * font_size
+    pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [x, y, x - dx, y, x - dx, y - dy, x, y - dy]),
+                         ('c3B', (200,200,200) * 4))
+
+    label = pyglet.text.Label('Stop Bot',
+                              font_name='Times New Roman',
+                              font_size=font_size,
+                              color=(0, 0, 0, 255),
+                              x=GAME_WIDTH + UI_WIDTH // 2, y=GAME_HEIGHT - 7 * font_size,
+                              anchor_x='center', anchor_y='center')
+    label.draw()
+
+    if last_move == 1:
+        x = GAME_WIDTH + (UI_WIDTH + font_size) / 2
+        y = GAME_HEIGHT - 10 * font_size
+        dx = 11
+        dy = 11
+    elif last_move == 2:
+        x = GAME_WIDTH + (UI_WIDTH - font_size) / 2
+        y = GAME_HEIGHT - 10 * font_size
+        dx = 11
+        dy = 11
+    elif last_move == 3:
+        x = GAME_WIDTH + (UI_WIDTH + 2 * font_size) / 2
+        y = GAME_HEIGHT - 9 * font_size
+        dx = 11
+        dy = 11
+    if last_move == 1 or last_move == 2 or last_move == 3:
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', [x, y, x - dx, y, x - dx, y - dy, x, y - dy]),
+                             ('c3B', (255,255,0) * 4))
 
 
 pyglet.clock.schedule(update_frame, 1 / 10.0)

train.py
@@ -18,8 +18,8 @@ class Policy(nn.Module):
         self.state_space = game.state_size
         self.action_space = game.action_size
 
-        self.l1 = nn.Linear(self.state_space, 128, bias=False)
-        self.l2 = nn.Linear(128, self.action_space, bias=False)
+        self.l1 = nn.Linear(self.state_space, 360, bias=False)
+        self.l2 = nn.Linear(360, self.action_space, bias=False)
 
         self.gamma = gamma
 
@@ -98,7 +98,6 @@ def train(episodes, save=0):
     running_reward = 1
     for episode in range(episodes):
         state = game.reset()  # Reset environment and record the starting state
-        done = False
         f = True
 
         game_reward = 0
@@ -108,6 +107,11 @@ def train(episodes, save=0):
             f = False
             # Step through environment using chosen action
             state, reward, done = game.step(action.item())
+
+            if action.item() == 1:
+                x = 0
+            if action.item() == 2:
+                x = 0
 
             # Save reward
             policy.reward_episode.append(reward)
@@ -128,4 +132,5 @@ def train(episodes, save=0):
             torch.save(policy.state_dict(), PATH)
 
 
-train(episodes=10000, save=1000)
+if __name__== "__main__":
+    train(episodes=10000, save=500)